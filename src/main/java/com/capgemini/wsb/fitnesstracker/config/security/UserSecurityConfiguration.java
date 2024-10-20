8dcef85d41cd86412bebebfca621bbcfb199a3f9
IgorKa <igor.karlik2973@gmail.com>
2024-10-20 18:58:12 +0200

    Implement Basic Auth (Spring Security)
    Update Postman collections for testing

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/config/security/UserSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.capgemini.wsb.fitnesstracker.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class UserSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/actuator/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/**").authenticated()
+                                .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
